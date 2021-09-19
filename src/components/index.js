75e1cfd9e3cf565ab292fc2422c689db0f1befc7
JiyangXu <xuyang1006@hotmail.com>
2021-09-19 12:53:12 +1000

    ignore added

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 export { default as Navbar } from './Navbar';
+export { default as CryptoDetails } from './Navbar';
+export { default as Cryptocurrencies } from './Navbar';
+export { default as Exchange } from './Navbar';
+export { default as Homepage } from './Navbar';
+export { default as News } from './Navbar';
+
